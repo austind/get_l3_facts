8aa8f2de2dca3643d6784f2462f397da8ff92d41
Austin de Coup-Crank <austindcc@gmail.com>
2019-10-16 10:42:12 -0700

    Blackened

--- get_l3_facts.py
+++ get_l3_facts.py
@@ -4,19 +4,23 @@ import napalm
 import getpass
 import ipaddress
 
+
 def arg_list(string):
-    return string.split(',')
+    return string.split(",")
+
 
 def open_device(host, driver, username, password, secret, ssh_config):
+    """ Opens connection to host and returns NAPALM object """
     napalm_driver = napalm.get_network_driver(driver)
     device = napalm_driver(
         hostname=host,
         username=username,
         password=password,
-        optional_args={'secret': secret, "ssh_config_file": ssh_config},
+        optional_args={"secret": secret, "ssh_config_file": ssh_config},
     )
     device.open()
     return device
+
 
 def get_iface_facts(device):
     """ Returns interface facts for a single device """
@@ -27,54 +31,70 @@ def get_iface_facts(device):
         ip_attrs = ifaces_ip.get(iface)
         if ip_attrs:
             attrs.update(ifaces_ip.get(iface))
-            ipv4 = attrs.get('ipv4')
+            ipv4 = attrs.get("ipv4")
             for address, prefix in ipv4.items():
-                prefix_length = prefix.get('prefix_length')
-                cidr = '{}/{}'.format(address, prefix_length)
+                prefix_length = prefix.get("prefix_length")
+                cidr = "{}/{}".format(address, prefix_length)
                 addr = ipaddress.ip_interface(cidr)
                 network = str(addr.network)
                 netmask = str(addr.netmask)
-                results.append({
-                    'device': device.hostname,
-                    'interface': iface,
-                    'description': attrs.get('description'),
-                    'address': address,
-                    'prefix_length': prefix_length,
-                    'cidr': cidr,
-                    'network': network,
-                    'netmask': netmask,
-                })
+                results.append(
+                    {
+                        "device": device.hostname,
+                        "interface": iface,
+                        "description": attrs.get("description"),
+                        "address": address,
+                        "prefix_length": prefix_length,
+                        "cidr": cidr,
+                        "network": network,
+                        "netmask": netmask,
+                    }
+                )
     return results
+
 
 def save_csv(csv_path, output):
     """ Saves output to CSV """
     fieldnames = [
-        'device',
-        'interface',
-        'address',
-        'prefix_length',
-        'cidr',
-        'netmask',
-        'network',
-        'description',
+        "device",
+        "interface",
+        "address",
+        "prefix_length",
+        "cidr",
+        "netmask",
+        "network",
+        "description",
     ]
-    with open(csv_path, 'w', newline='') as f:
+    with open(csv_path, "w", newline="") as f:
         writer = csv.DictWriter(f, fieldnames=fieldnames)
         writer.writeheader()
         writer.writerows(output)
+
 
 def main():
     parser = argparse.ArgumentParser(
         description="Retrieve L3 interface info from a network device"
     )
-    parser.add_argument("--hosts", "-H", help="Comma-delimited list of IPs and/or FQDNs to query", required=True, type=arg_list)
     parser.add_argument(
-        "--username", "-u", help="Username", default=getpass.getuser())
+        "--hosts",
+        "-H",
+        help="Comma-delimited list of IPs and/or FQDNs to query",
+        required=True,
+        type=arg_list,
+    )
+    parser.add_argument("--username", "-u", help="Username", default=getpass.getuser())
+    parser.add_argument("--password", "-p", help="Password", default=getpass.getpass())
     parser.add_argument(
-        "--password", "-p", help="Password", default=getpass.getpass())
-    parser.add_argument('--secret', '-s', help='Enable secret', default=getpass.getpass('Secret: '))
-    parser.add_argument('--output', '-o', help='Save as CSV to this path', dest='csv_path', required=True)
-    parser.add_argument('--driver', help='NAPALM network driver to use', default='ios')
+        "--secret", "-s", help="Enable secret", default=getpass.getpass("Secret: ")
+    )
+    parser.add_argument(
+        "--output",
+        "-o",
+        help="Save as CSV to this path",
+        dest="csv_path",
+        required=True,
+    )
+    parser.add_argument("--driver", help="NAPALM network driver to use", default="ios")
     parser.add_argument(
         "--ssh-config",
         "-c",
@@ -83,14 +103,22 @@ def main():
         dest="ssh_config",
     )
     args = parser.parse_args()
-    
+
     results = []
     for host in args.hosts:
-        device = open_device(host, args.driver, args.username, args.password, args.secret, args.ssh_config)
+        device = open_device(
+            host,
+            args.driver,
+            args.username,
+            args.password,
+            args.secret,
+            args.ssh_config,
+        )
         iface_facts = get_iface_facts(device)
         device.close()
         results = results + iface_facts
     save_csv(args.csv_path, results)
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     main()
